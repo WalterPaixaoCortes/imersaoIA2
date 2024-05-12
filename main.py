985ea8861344cc874b3a6e01354cb1f7e4da8977
Walter Ritzel Paixao Cortes <wpcortes@gmail.com>
2024-05-11 22:44:18 -0300

    versão inicial

---
+++ main.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+import os
+
+from taipy.gui import Gui
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from pages.generate import gen_q_md
+from pages.visualize import tbl_q_md
+from pages.report import sum_q_md
+
+pages = {
+    "/": "<center><|navbar|></center>",
+    "Gerar": gen_q_md,
+    "Visualizar": tbl_q_md,
+    "Sumario": sum_q_md,
+}
+
+
+# ----------------------------------------------------------------
+# Main
+# ----------------------------------------------------------------
+if __name__ == "__main__":
+    Gui(pages=pages).run(
+        title="EduAssist.ai - Gerador de Questões",
+        host="0.0.0.0",
+        port=os.getenv("PORT"),
+        use_reloader=True,
+    )
